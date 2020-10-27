28095c9e5a73720eb6029cc903f7a78f458bfec2
Michael Carroll <michaelcarroll1999@gmail.com>
2020-10-27 14:01:54 -0400

    functioning favicon

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ app.use(helmet())
 app.use(compression())
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(multer({storage: fileStorage, fileFilter: fileFilter}).single('image'))
+app.use(multer({storage: fileStorage, fileFilter: fileFilter}).single('image'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/images/favicon', express.static(path.join(__dirname, 'images/favicon')));
 
